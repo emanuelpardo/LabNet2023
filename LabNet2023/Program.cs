3660c8cd8fad67cec38a5c8f75b641ef2e412922
Emanuel Pardo <emanuel.pardo89@gmail.com>
2023-02-28 00:00:33 -0300

    Primer entrega labnet2023

---
+++ LabNet2023/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNet2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello World\nApreta enter para cerrar**");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + " - " + ex.StackTrace);
+            }
+           
+        }
+    }
+}
