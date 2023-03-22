124aab4b50dc07251663310e0978aa2256c2581b
Emanuel Pardo <emanuel.pardo89@gmail.com>
2023-03-22 00:20:06 -0300

    PrimerVersion MVC labnetpractica6

---
+++ labNetPractica3/MVC/Controllers/SuppliersController.cs
@@ -0,0 +1,21 @@
+﻿using BE;
+using BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        // GET: Suppliers
+        public ActionResult Index()
+        {
+            BL_Suppliers _blsuppliers = new BL_Suppliers();
+            List<Suppliers> Lista_suppliers = _blsuppliers.GetListAll();
+            return View(Lista_suppliers);
+        }
+    }
+}
---
+++ labNetPractica3/MVC/Models/SuppliersViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class SuppliersViewModel
+    {
+
+        public string NombreCompania { get; set; }
+        public string NombreContacto { get; set; }
+        public string Ciudad { get; set; }
+    }
+}
