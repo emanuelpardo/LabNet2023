89325523ec82bebb161a13b638423a2a30df93ac
Emanuel Pardo <emanuel.pardo89@gmail.com>
2023-03-24 20:50:19 -0300

    -v1 labnetpractica6

--- labNetPractica3/MVC/Controllers/SuppliersController.cs
+++ labNetPractica3/MVC/Controllers/SuppliersController.cs
@@ -1,5 +1,6 @@
 ﻿using BE;
 using BL;
+using MVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,100 @@ namespace MVC.Controllers
 {
     public class SuppliersController : Controller
     {
-        // GET: Suppliers
+        private const string index = "Index";
+        private BL_Suppliers _blsuppliers;
+        public SuppliersController()
+        {
+            _blsuppliers = new BL_Suppliers();
+        }
+       
+        [HttpGet]
         public ActionResult Index()
         {
-            BL_Suppliers _blsuppliers = new BL_Suppliers();
-            List<Suppliers> Lista_suppliers = _blsuppliers.GetListAll();
-            return View(Lista_suppliers);
+            try
+            {
+                return View(_blsuppliers.GetListAll().Select(s => new SuppliersViewModel { ID = s.SupplierID, NombreCompania = s.CompanyName, NombreContacto = s.ContactName, Ciudad = s.City }).OrderBy(x => x.ID).ToList());
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+           
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            try
+            {
+                if (id != null)
+                    _blsuppliers.Delete(new Suppliers { SupplierID = id.Value });
+
+                return RedirectToAction(index);
+            }
+            catch (Exception ex)
+            {
+
+                return Content(ex.Message);
+            }
+          
+        }
+
+        [HttpGet]
+        public ActionResult InsertEdit(int? id)
+        {
+            SuppliersViewModel Spw = new SuppliersViewModel();
+            try
+            {
+                if (id != null)
+                {
+                    var s = _blsuppliers.GetListAll().Where(x => x.SupplierID == id.Value).FirstOrDefault();
+                    Spw = new SuppliersViewModel
+                    {
+                        ID = s.SupplierID,
+                        NombreCompania = s.CompanyName,
+                        NombreContacto = s.ContactName,
+                        Ciudad = s.City
+                    };
+                }
+
+                return View(Spw);
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+            
+        }
+
+        [HttpPost]
+        public ActionResult Guardar(SuppliersViewModel Svw)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    if (Svw.ID != null)
+                    {
+                        var s = _blsuppliers.GetListAll().Where(x => x.SupplierID == Svw.ID.Value).FirstOrDefault();
+                        s.CompanyName = Svw.NombreCompania;
+                        s.ContactName = Svw.NombreContacto;
+                        s.City = Svw.Ciudad;
+                        _blsuppliers.Modify(s);
+                    }
+                    else
+                        _blsuppliers.Insert(new Suppliers { CompanyName = Svw.NombreCompania, ContactName = Svw.NombreContacto, City = Svw.Ciudad });
+                   
+                }
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+            return RedirectToAction(index);
+
+
         }
     }
 }
--- labNetPractica3/MVC/Models/SuppliersViewModel.cs
+++ labNetPractica3/MVC/Models/SuppliersViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,37 @@ namespace MVC.Models
 {
     public class SuppliersViewModel
     {
+        private int? id;
+        private string nombreCompania;
+        private string nombreContacto;
+        private string ciudad;
 
-        public string NombreCompania { get; set; }
-        public string NombreContacto { get; set; }
-        public string Ciudad { get; set; }
+        public int? ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [Required(ErrorMessage = "Nombre de Compañía es obligatorio")]
+        [StringLength(40, ErrorMessage = "Agregue un caracter")]
+        public string NombreCompania
+        {
+            get { return nombreCompania; }
+            set { nombreCompania = value; }
+        }
+       
+        public string NombreContacto
+        {
+            get { return nombreContacto; }
+            set { nombreContacto = value; }
+        }
+        public string Ciudad
+        {
+            get { return ciudad; }
+            set { ciudad = value; }
+        }
+
+
+
     }
 }
