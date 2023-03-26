f354f016eb086595ae8218c251735428422abb22
Emanuel Pardo <emanuel.pardo89@gmail.com>
2023-03-26 19:21:31 -0300

    entrega labnetpractica7: -Consumir api desde capa MVC -CRUD en capa APIWEB

---
+++ labNetPractica3/MVC/Controllers/RickandMortyAPIController.cs
@@ -0,0 +1,59 @@
+﻿using MVC.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class RickandMortyAPIController : Controller
+    {
+        // GET: RickandMortyAPI
+        public ActionResult Index()
+        {
+            const string results = "results";
+            List<RickAndMortyCharacterViewModel> Lista_Personajes = new List<RickAndMortyCharacterViewModel>();
+            try
+            {
+                string url = "https://rickandmortyapi.com/api/character";
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+                HttpClient _httpClient = new HttpClient();
+                _httpClient.DefaultRequestHeaders.Clear();
+                _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                ServicePointManager.ServerCertificateValidationCallback = delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
+                
+                HttpResponseMessage response = _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).Result;
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    string content = response.Content.ReadAsStringAsync().Result;
+                    if (!string.IsNullOrEmpty(content))
+                    {
+                        JObject jObject = JObject.Parse(content);
+                        JToken jt = jObject.GetValue(results);
+                        Lista_Personajes = JsonConvert.DeserializeObject<List<RickAndMortyCharacterViewModel>>(jt.ToString());
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                this.Volver();
+            }
+
+            return View(Lista_Personajes);
+        }
+
+        public ActionResult Volver()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ labNetPractica3/MVC/Models/RickAndMortyCharacterViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class RickAndMortyCharacterViewModel
+    {
+
+        [DataMember(Name = "id")]
+        public int? Id { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+        
+        [DataMember(Name = "status")]
+        public string Status { get; set; }
+
+        [DataMember(Name = "species")]
+        public string Species { get; set; }
+
+        [DataMember(Name = "gender")]
+        public string Gender { get; set; }
+
+        [DataMember(Name = "image")]
+        public string Image { get; set; }
+
+        [DataMember(Name = "created")]
+        public DateTime? Created { get; set; }
+    }
+}
